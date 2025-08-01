3c53253b24e6e7ae275a91131d14751a984b0ee0
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-07-31 21:18:48 -0300

    feat: entity claims

--- ETL/wikidata-integrity.py
+++ ETL/wikidata-integrity.py
@@ -9,3 +9,14 @@ def get_wikidata_info(entity_id):
         'description': entity.description,
         'claims': {}
     }
+    
+    for prop, value in entity.attributes.get('claims', {}).items():
+        prop_label = client.get(prop).label
+        data['claims'][prop_label] = []
+        for v in value:
+            if isinstance(v.get('mainsnak', {}).get('datavalue', {}).get('value', {}), dict):
+                if 'id' in v['mainsnak']['datavalue']['value']:
+                    item = client.get(v['mainsnak']['datavalue']['value']['id'])
+                    data['claims'][prop_label].append(item.label)
+    
+    return data
